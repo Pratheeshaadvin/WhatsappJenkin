4f484b6cac2926bfb08b8117204a2af8e53037b7
Pratheeshaadvin <135798854+Pratheeshaadvin@users.noreply.github.com>
2023-09-22 11:19:51 +0530

    jenkin video6

--- Hello_World.java
+++ Hello_World.java
@@ -4,6 +4,7 @@ public static void main (String[] args){
 System.out.println("Hello Pratheesha");
 System.out.println("Hello Advin");
 System.out.println("Hello Ahira");  
-System.out.println("Try branch"); 
+System.out.println("Try branch");
+  System.out.println("jenkin built via git");
 }
 }
